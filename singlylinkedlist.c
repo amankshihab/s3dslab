ee75946b20059174e40eb2a4098b35d0e4c37ade
Aman K Shihab <amanshihab276@gmail.com>
2020-10-23 19:38:42 +0530

    Added singlylinkedlist.c

--- singlylinkedlist.c
+++ singlylinkedlist.c
@@ -4,38 +4,90 @@
 struct node{
 
     int data;
-    struct node *next;
+    struct node *link;
 };
 
-struct node *head = NULL;
+struct node *start = NULL, *temp;
 
-void main(){
+void display(){
 
-    struct node *newnode;
+    if(start == NULL){
     
-    printf("Enter the value to be entered:");
-    int e;
-    scanf("%d", &e);
-
-    newnode = (struct node *)malloc(sizeof(struct node));
-
-    if(head = NULL){
-
-        newnode -> next = NULL;
-        head = newnode;
+        printf("\nLinked list is empty!\n");
     }
     else{
 
-        newnode -> next = head;
-        head = newnode; 
+        printf("\nThe linked list now is:\n");
+        
+        temp = start;
+        
+        while(temp != NULL){
+
+            printf("%d  ", temp -> data);
+            temp = temp -> link;
+        }
     }
+}
 
-    struct node *temp = head;
+void insert(){
 
-    while(temp -> next != NULL){
-        printf("\n%d", temp -> data);
-        temp = temp -> next;
+    int el, pos;
+    
+    printf("\nEnter the element to insert:");
+    scanf("%d", &el);
+
+    struct node *p;
+    p = malloc(sizeof(struct node));
+
+    //printf("%d", 1);
+    
+    if(start == NULL){
+
+        p -> data = el;
+        p -> link = NULL;
+        start = p;
+
+        display();
     }
+    else{
 
-    //printf("\n%d --->NULL",temp->data);
+        printf("\nEnter position:");
+        scanf("%d", &pos);
+
+        temp = start;
+
+        for(int i = 0; i < (pos - 1); i++){
+
+            temp = temp -> link;
+        }
+
+        p -> link = temp -> link;
+        temp -> link = p;
+
+        display();
+    }
+}
+
+void main(){
+
+    int ans = 0;
+    
+    while(1){
+
+        printf("\n\nLinked List Operations:\n\n1.Insert beginning\n2.Delete\n3.Display\n4.Exit\n\nEnter your option:");
+        scanf("%d", &ans);
+
+        switch(ans){
+
+            case 1 : insert();
+                    break;
+            //case 2 : delete();
+                    //break;
+            case 3 : display();
+                    break;
+            case 4 : exit(0);
+                    break;
+            default : printf("\n!Wrong option!\n");
+        }
+    }
 }
