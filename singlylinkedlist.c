c27038c2829ba1a25f3f3939b7d346d9a3ad59d3
Aman K Shihab <amanshihab276@gmail.com>
2020-10-27 21:01:11 +0530

    Updated singlylinkedlist.c

--- singlylinkedlist.c
+++ singlylinkedlist.c
@@ -7,7 +7,7 @@ struct node{
     struct node *link;
 };
 
-struct node *start = NULL, *temp;
+struct node *start = NULL, *temp, *loc;     //loc for getting location of element
 
 void display(){
 
@@ -21,15 +21,27 @@ void display(){
         
         temp = start;
         
-        while(temp != NULL){
-
+        if(temp -> link == NULL){
+            
             printf("%d  ", temp -> data);
-            temp = temp -> link;
+        }
+        else{
+        
+            do{
+
+                printf("%d  ", temp -> data);
+                temp = temp -> link;
+
+                if(temp -> link == NULL){
+            
+                    printf("%d  ", temp -> data);
+                }
+            }while(temp -> link != NULL);
         }
     }
 }
 
-void insert(){
+void insert_beg(){
 
     int el, pos;
     
@@ -37,35 +49,79 @@ void insert(){
     scanf("%d", &el);
 
     struct node *p;
-    p = malloc(sizeof(struct node));
+    p = (struct node*)malloc(sizeof(struct node));
 
-    //printf("%d", 1);
     
-    if(start == NULL){
+    p -> data = el;
+    
+    if(start == NULL)
+    p -> link = NULL;
+    else
+    p -> link = start;
+    
+    start = p;
 
-        p -> data = el;
-        p -> link = NULL;
-        start = p;
+    display();
+}
 
-        display();
-    }
+void insert_at_pos(){
+
+    struct node *p;
+    p = (struct node*)malloc(sizeof(struct node));
+    
+    printf("Enter the position to enter:");
+    int ele, pos;
+    scanf("%d", &pos);
+
+    if(pos == 1)
+    insert_beg();
     else{
 
-        printf("\nEnter position:");
-        scanf("%d", &pos);
+        printf("Enter the element to be inserted:");
+        scanf("%d", &ele);
+
+        p -> data = ele;
 
         temp = start;
 
-        for(int i = 0; i < (pos - 1); i++){
+        for(int i = 1; i < (pos - 1); i++){
 
             temp = temp -> link;
         }
-
         p -> link = temp -> link;
         temp -> link = p;
-
         display();
     }
+}
+
+void insert_end(){
+
+    struct node *p;
+    p = (struct node*)malloc(sizeof(struct node));
+
+    printf("Enter the element to insert:");
+    int item;
+    scanf("%d", &item);
+
+    p -> data = item;
+    if(start == NULL){
+
+        p -> link = NULL;
+        start = p;
+    }
+    else{
+
+        temp = start;
+        while(temp -> link != NULL){
+
+            temp = temp -> link;
+        }
+        temp -> link = p;
+        p -> link = p;
+        p -> link = NULL;
+    }
+
+    display();
 }
 
 void main(){
@@ -74,18 +130,20 @@ void main(){
     
     while(1){
 
-        printf("\n\nLinked List Operations:\n\n1.Insert beginning\n2.Delete\n3.Display\n4.Exit\n\nEnter your option:");
+        printf("\n\nLinked List Operations:\n\n1.Insert beginning\n2.Insert at end\n3.Insert in the middle\n4.Delete beginning\n5.Delete end\n6.Delete in the middle\n7.Display\n8.Exit\n\nEnter your option:");
         scanf("%d", &ans);
 
         switch(ans){
 
-            case 1 : insert();
+            case 1 : insert_beg();
                     break;
-            //case 2 : delete();
-                    //break;
-            case 3 : display();
+            case 2 : insert_end();
+                    break;
+            case 3 : insert_at_pos();
                     break;
             case 4 : exit(0);
+                    break;
+            case 8 : exit(0);
                     break;
             default : printf("\n!Wrong option!\n");
         }
