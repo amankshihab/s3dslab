460c46d0c25323d855b30a368e09026e04f28b06
Aman K Shihab <amanshihab276@gmail.com>
2020-12-06 23:34:58 +0530

    added bintree.c

--- bintreearray.c
+++ bintreearray.c
@@ -1,87 +1,200 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int tree[30], nodes;
+int tree[30], nodes, last, count = 10;
 
-void display(){
+void display(int root, int space){
 
-    if(tree[1] == '\0'){
-
-        printf("Tree is empty!");
-    }
+    if(tree[root] == '\0')
+        return;
     
-    else{
-        
-        for(int i = 1; i <= nodes; i++){
+    space += count;
 
-            //printf("%d", tree[i]);
+    display(2*root+1, space);
 
-            if(tree[i] != '\0')
-                printf("%d", tree[i]);
-            else
-                break;
+    printf("\n");
+
+    for(int i = count; i < space; i++)
+        printf(" ");
+    printf("%d\n", tree[root]);
+
+    display(2*root, space);
+}
+
+int search(int index, int key){
+
+    int i = index, x;
+    
+    if(tree[i] == key)
+        return i;
+    if(2*i <= last)
+        x = search(2*i, key);
+    else
+        return 0;
+
+    if((x == 0) && (2*i+1 <= last))
+        x = search(2*i+1, key);
+
+    return x;
+}
+
+void insertion(int key, int item){
+
+    int l;
+    char b;
+    l = search(1, key);
+
+    if(l == 0)
+        printf("Search Unsuccessful!");
+    else if(tree[2*l] == '\0' || tree[2*l+1] == '\0'){
+
+        printf("Enter to left or right (l or r):");
+        scanf("%c%c", &b, &b);
+
+        if(b == 'l' || b == 'L'){
+
+            if(tree[2*l] == '\0'){
+
+                tree[2*l] = item;
+                printf("Inserted as %d node.", 2*l);
+                last = 2 * l + 1;
+            }
+            else{
+
+                printf("Insertion to node %d not possible!", l);
+            }
+        }
+        else if(b == 'r' || b == 'R'){
+
+            if(tree[2*l+1] == '\0'){
+
+                tree[2*l+1] = item;
+                printf("Inserted as %d node.", 2*l+1);
+                last = 2 * l + 1;
+            }
+            else{
+
+                printf("Insertion to node %d not possible!", l);
+            }
+        }
+        else{
+
+            printf("Insertion not possible!");
         }
     }
 }
 
-void root(int r){
+void build_tree(int i, int item){
 
-    tree[1] = r;
+    if(i != 0){
+
+        last = i;
+
+        tree[i] = item;
+        printf("Node %d has left subtree(y/n):", i);
+        char ch;
+        scanf("%c%c",&ch,&ch);
+        if(ch == 'y'){
+            
+            printf("Enter the data to insert:");
+            int dt;
+            scanf("%d", &dt);
+            build_tree(2*i, dt);
+        }
+        else{
+
+            build_tree(0, '\0');
+        }
+
+        printf("Node %d has right subtree(y/n):",i);
+        char c;
+        scanf("%c%c",&c,&c);
+        if(c == 'y'){
+
+            printf("Enter the data to insert:");
+            int g;
+            scanf("%d", &g);
+            build_tree(2*i+1, g);
+        }
+        else{
+
+            build_tree(0, '\0');
+        }
+
+    }
+}
+void inorder(int i){
+
+    if(tree[i] != '\0'){
+
+        inorder(2*i);
+        printf("%d  ", tree[i]);
+        inorder(2*i+1);
+    } 
 }
 
-void lchild(){
+void postorder(int i){
 
-    printf("\nEnter the parent nodes position:");
-    int parent;
-    scanf("%d", &parent);
+    if(tree[i] != '\0'){
 
-    printf("Enter the data:");
-    int data;
-    scanf("%d", &data);
-
-    tree[(2*parent)] = data;
+        postorder(2*i);
+        postorder(2*i+1);
+        printf("%d  ", tree[i]);
+    }
 }
 
-void rchild(){
+void preorder(int i){
 
-    printf("\nEnter the parent node:");
-    int parent;
-    scanf("%d", &parent);
+    if(tree[i] != '\0'){
 
-    printf("Enter the data:");
-    int data;
-    scanf("%d", &data);
-
-    tree[(2*parent + 1)] = data;
+        printf("%d  ", tree[i]);
+        preorder(2*i);
+        preorder(2*i+1);
+    }
 }
 
 void main(){
 
-    printf("Enter the no. of nodes in the tree:");
-    scanf("%d", &nodes);
+    int ch, root, key, item;
+
+    printf("\nEnter the root element:");
+    scanf("%d", &root);
     
-    printf("Insert root :");
-    int r;
-    scanf("%d", &r);
-    root(r);
+    build_tree(root, 1);
+    
+    printf("%d", last);
+    
+    for(int i = 1; i <=last; i++)
+        printf("%d", tree[i]);
     
     while(1){
 
-        printf("\nBinary tree using array:\n\n1.Insert left\n2.Insert right\n3.Display\n4.Delete left\n5.Delete right\n6.Exit\n\nEnter your choice:");
-        int ch;
+        printf("\nBinary Tree Operations:\n\n1.Inorder Traversal\n2.Postorder Traversal\n3.Preorder Traversal\n4.Insertion\n5.Deletion\n6.Exit\n\nEnter your choice:");
         scanf("%d", &ch);
 
         switch(ch){
 
-            case 1 : lchild();
+            case 1 : printf("\nInorder: ");
+                     inorder(1);
                         break;
-            case 2 : rchild();
+            case 2 : printf("\nPostorder: ");
+                     postorder(1);
                         break;
-            case 3 : display();
+            case 3 : printf("\nPreorder: ");
+                     preorder(1);
                         break;
-            case 6 : exit(0);
+            case 4 : printf("Enter the position and item:");
+                     scanf("%d%d", &key, &item);
+                     insertion(key, item);
+                     printf("%d", last);
+                     for(int i = 1; i <=last; i++)
+                        printf("%d", tree[i]);
                         break;
-            default : printf("\n!!WRONG OPTION!!");
+            case 6 : exit(1);
+                        break;
+            case 8 : display(1, 1);
+                        break;
+            default : printf("\n!!WRONG OPTIONS!!\n");
         }
     }
 }
