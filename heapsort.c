806b6a3c5c9793650829bb86c1a37560b0d64aa4
Aman K Shihab <amanshihab276@gmail.com>
2021-01-06 12:33:23 +0530

    heapsort success

--- heapsort.c
+++ heapsort.c
@@ -72,8 +72,6 @@ void deletion(){
     else{
 
         swap(&heap[1], &heap[size]);
-
-        display(1,1);
         
         heap[size] = '\0';
         size -= 1;
@@ -114,6 +112,7 @@ int main(){
         scanf("%d", &heap[i]);
 
     maxheap(1, size);
+    maxheap(1,size);
 
     printf("\nThe entered array in maxheap form:\n");
     display(1,1);
@@ -127,15 +126,22 @@ int main(){
         switch(ch){
 
             case 1: insertion();
+                    maxheap(1,size);
                         break;
 
             case 2: deletion();
+                    maxheap(1,size);
+                    display(1,1);
                         break;
             
             case 3: heapsort(1, size);
                     printf("\nThe heap after heap sort is:");
                     display(1,1);
+                    printf("\nSorted heap: ");
+                    for(int i = 1; i < size; i++)
+                        printf("%d  ", heap[i]);
                     maxheap(1, size);
+                    maxheap(1,size);
                         break;
             
             case 4: display(1, 1);
